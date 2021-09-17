4bcfcc7e0f631d8a3d5899020b9448c7a4c166e1
Anurag-Bharati <anuragbharati26@gmail.com>
2021-09-18 00:59:06 +0545

    PersonalDetails

--- src/main/java/Manager/PersonalDataField.java
+++ src/main/java/Manager/PersonalDataField.java
@@ -1,10 +1,19 @@
 package Manager;
 
+/**
+ * <h2>Contains Sensitive Data</h2>
+ * <p>API_TOKEN - is token to use the API<br>
+ * MASTER_GMAIL - is sender's gmail<br>
+ * MASTER_PASS - is sender's gmail password<br>
+ * DATABASE_USER - is database user name<br>
+ * DATABASE_PASS - is database password<br>
+ * </p>
+ * @apiNote The weather provider used is OpenWeather
+ */
 public class PersonalDataField {
     public static final String API_TOKEN = "API_TOKEN_HERE";
     public static final String MASTER_GMAIL = "GMAIL_ACCOUNT";
     public static final String MASTER_PASS = "GMAIL_PASS";
     public static final String DATABASE_USER = "DATABASE_USER";
     public static final String DATABASE_PASS = "DATABASE_PASS";
-
 }
