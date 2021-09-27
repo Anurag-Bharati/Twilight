ee227cde60cf6d5218c4ad3c10d217b416651720
Anurag-Bharati <anuragbharati26@gmail.com>
2021-09-27 21:42:22 +0545

    Final Final Push

--- src/main/java/Model/PersonalDataField.java
+++ src/main/java/Model/PersonalDataField.java
@@ -11,9 +11,9 @@ package Model;
  * @apiNote The weather provider used is OpenWeather
  */
 public class PersonalDataField {
-    public static final String API_TOKEN = "30c06d6d08ad5c52058bcf778d1fc841";
-    public static final String MASTER_GMAIL = "noreply.thisisforaproject@gmail.com";
-    public static final String MASTER_PASS = "54321012345";
-    public static final String DATABASE_USER = "root";
-    public static final String DATABASE_PASS = "root";
+    public static final String API_TOKEN = "API_KEY_HERE";
+    public static final String MASTER_GMAIL = "GMAIL_HERE";
+    public static final String MASTER_PASS = "GMAIL_PASS_HERE";
+    public static final String DATABASE_USER = "DB_USER_HERE";
+    public static final String DATABASE_PASS = "DB_PASS_HERE";
 }
